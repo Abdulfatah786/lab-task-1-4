8e3dffb7091fc11d6ffefe25b63e253bacd727e8
Abdulfatah786 <117835306+Abdulfatah786@users.noreply.github.com>
2022-11-10 13:36:18 +0500

    Add files via upload

---
+++ task 3.java
@@ -0,0 +1,17 @@
+public class Main {
+	public static void main(String[] args) {
+		
+		int a = 55,b=60;
+		int c=a+b;
+		int d=a-b;
+		int e=a*b;
+		int f=a%b;
+		
+		System.out.println("c  =  " +c);
+		System.out.println("d  =  " +d);
+		System.out.println("e  =  " +e);
+		System.out.println("f =  " +f);
+		
+		
+	}
+}
